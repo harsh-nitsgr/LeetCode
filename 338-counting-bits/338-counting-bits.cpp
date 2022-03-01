51561c3f6cc8a8255c8a0007414e30babf798b0d
harsh-nitsgr <91006836+harsh-nitsgr@users.noreply.github.com>
2022-03-01 00:35:58 -0800

    Time: 7 ms (62.32%), Space: 7.8 MB (89.06%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         
-        vector<int>binary(n+1);
-        binary[0]=0;
+        vector<int>binary(n+1,0);
         
         // n & (n-1) removes least significant bit
         for(int i=1;i<=n;i++){
