c97e949821d5c8dbbbd4b1a25dbc0b819420ff55
harsh-nitsgr <91006836+harsh-nitsgr@users.noreply.github.com>
2022-03-16 09:12:33 -0700

    Time: 17 ms (29.18%), Space: 15 MB (100.00%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -1,15 +1,26 @@
+// class Solution {
+// public:
+//     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+//         // return 1;
+//         int n=pushed.size();
+//         stack<int>s;
+        
+//         for(int i=0,j=0;i<n;i++){
+//             s.push(pushed[i]);
+//             while(!s.empty() and s.top()==popped[j])s.pop(),j++;
+//         }
+//         return s.empty();
+//     }
+// };
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        // return 1;
-        int n=pushed.size();
-        stack<int>s;
         
-        for(int i=0,j=0;i<n;i++){
-            s.push(pushed[i]);
-            while(!s.empty() and s.top()==popped[j])s.pop(),j++;
+        int i=0,j=0;
+        for(auto &it:pushed){
+            pushed[i++]=it;
+            while(i>0 and pushed[i-1]==popped[j]) i--,j++;
         }
-        cout<<s.size();
-        return s.empty();
+        return i==0;
     }
 };
