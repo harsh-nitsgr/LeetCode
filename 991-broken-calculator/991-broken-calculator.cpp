edb0b9d8833ee55552bcd687bdc774f6116861f8
harsh-nitsgr <91006836+harsh-nitsgr@users.noreply.github.com>
2022-03-23 23:21:30 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (73.18%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -3,11 +3,11 @@ public:
     int brokenCalc(int sv, int target) {
         
         int count=0;
-        while(sv!=target){
+        while(sv<target){
             if(target % 2==0 and target>sv) target/=2;
             else target+=1;
             count++;
         }
-        return count;
+        return count+(sv-target);
     }
 };
