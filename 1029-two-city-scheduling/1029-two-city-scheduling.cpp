7b1d827c82fbc22c05e8d123eda3091963c60116
harsh-nitsgr <91006836+harsh-nitsgr@users.noreply.github.com>
2022-03-25 18:46:55 +0530

    Time: 16 ms (12.30%), Space: 10.1 MB (7.87%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
+    //depends on the differnce between costs of travelling thorugh a and b
+    static bool cmp(vector<int>a,vector<int>b){
+        return a[1]-a[0]<b[1]-b[0];
+    }
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        vector<int>difference;
         int res=0;
-        for(auto &cost:costs){
-            difference.push_back(cost[1]-cost[0]);
-            res+=cost[0];
-        }
-        sort(difference.begin(),difference.end());
-        
-        for(int i=0;i<costs.size()/2;i++){
-            res+=difference[i];
+        sort(costs.begin(),costs.end(),cmp);
+        for(int i=0;i<costs.size();i++){
+            if(i<costs.size()/2)res+=costs[i][1];
+            else res+=costs[i][0];
         }
         return res;
     }
 };
+// class Solution {
+// public:
+//     int twoCitySchedCost(vector<vector<int>>& costs) {
+//         vector<int>difference;
+//         int res=0;
+//         for(auto &cost:costs){
+//             difference.push_back(cost[1]-cost[0]);
+//             res+=cost[0];
+//         }
+//         sort(difference.begin(),difference.end());
+        
+//         for(int i=0;i<costs.size()/2;i++){
+//             res+=difference[i];
+//         }
+//         return res;
+//     }
+// };
