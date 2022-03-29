44bbce4e8e974b03b1631dbf4d0b7b58a58d8a48
harsh-nitsgr <91006836+harsh-nitsgr@users.noreply.github.com>
2022-03-29 18:17:38 +0530

    Time: 14 ms (5.65%), Space: 11.1 MB (75.42%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,38 +1,57 @@
-class Solution {
-public:
-    
-    int binary_search(vector<int>&nums,int target,int s,int e){
-        
-        while(s<=e){
-            int mid=s+(e-s)/2;
-            if(nums[mid]==target)return mid;
-            if(nums[mid]<target)s=mid+1;
-            if(nums[mid]>target)e=mid-1;
-        }
-        return -1;
-    }
-    int searchUtil(vector<int>nums,int target,int start,int end){
-        if(start>end)return -1;
-        
-        int mid=start+(end-start)/2;
-        
-        if(nums[mid]==target)return mid;
-        if(nums[mid]<nums[start]){
-            if(target>nums[mid] and target<nums[end])
-                return binary_search(nums,target,mid,end);
-            return searchUtil(nums,target,start,mid);
-        }
-        if(nums[mid]>nums[start]){
-            if(target<nums[mid] and target>nums[start])
-                return binary_search(nums,target,start,mid);
-            return searchUtil(nums,target,mid,end);
-        }
-        return -1;
-    }
-    
+class Solution{
+    public:
     int search(vector<int>& nums, int target) {
-        if(nums[0]==target)return 0;
-        if(nums[nums.size()-1]==target)return nums.size()-1;
-        return searchUtil(nums,target,0,nums.size()-1);
+        int lo = 0, hi = nums.size();
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+
+            double num = (nums[mid] < nums[0]) == (target < nums[0])
+                       ? nums[mid]
+                       : target < nums[0] ? -INFINITY : INFINITY;
+
+            if (num < target)lo = mid + 1;
+            else if (num > target) hi = mid;
+            else return mid;
+        }
+        return -1;
     }
 };
+
+// class Solution {
+// public:
+    
+//     int binary_search(vector<int>&nums,int target,int s,int e){
+        
+//         while(s<=e){
+//             int mid=s+(e-s)/2;
+//             if(nums[mid]==target)return mid;
+//             if(nums[mid]<target)s=mid+1;
+//             if(nums[mid]>target)e=mid-1;
+//         }
+//         return -1;
+//     }
+//     int searchUtil(vector<int>nums,int target,int start,int end){
+//         if(start>end)return -1;
+        
+//         int mid=start+(end-start)/2;
+        
+//         if(nums[mid]==target)return mid;
+//         if(nums[mid]<nums[start]){
+//             if(target>nums[mid] and target<nums[end])
+//                 return binary_search(nums,target,mid,end);
+//             return searchUtil(nums,target,start,mid);
+//         }
+//         if(nums[mid]>nums[start]){
+//             if(target<nums[mid] and target>nums[start])
+//                 return binary_search(nums,target,start,mid);
+//             return searchUtil(nums,target,mid,end);
+//         }
+//         return -1;
+//     }
+    
+//     int search(vector<int>& nums, int target) {
+//         if(nums[0]==target)return 0;
+//         if(nums[nums.size()-1]==target)return nums.size()-1;
+//         return searchUtil(nums,target,0,nums.size()-1);
+//     }
+// };
