7fc3d3b58ccf7be925229d04eb09a9da5efa89c1
harsh-nitsgr <91006836+harsh-nitsgr@users.noreply.github.com>
2022-04-07 22:45:03 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (34.27%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>pq;
+        
+        for(auto&stone:stones)pq.push(stone);
+        while(pq.size()>1){
+            int t1=pq.top();
+            pq.pop();
+            int t2=pq.top();
+            pq.pop();
+            if(t1!=t2)pq.push(t1-t2);
+        }
+        if(pq.empty())return 0;
+        return pq.top();
+    }
+};
