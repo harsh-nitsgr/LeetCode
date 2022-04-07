7ce0f5f5024118d33fecd06173a62fbb53cde57a
harsh-nitsgr <91006836+harsh-nitsgr@users.noreply.github.com>
2022-04-07 22:48:09 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (78.96%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -5,13 +5,10 @@ public:
         
         for(auto&stone:stones)pq.push(stone);
         while(pq.size()>1){
-            int t1=pq.top();
-            pq.pop();
-            int t2=pq.top();
-            pq.pop();
-            if(t1!=t2)pq.push(t1-t2);
+            int t1=pq.top();pq.pop();
+            int t2=pq.top();pq.pop();
+            pq.push(t1-t2);
         }
-        if(pq.empty())return 0;
         return pq.top();
     }
 };
